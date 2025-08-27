04e17558352b7c2d3cef5b8865d6158e83e3f17f
Chris-verna <tuq60693@temple.edu>
2025-08-27 14:24:43 -0400

    Update print_random.c

--- print_random.c
+++ print_random.c
@@ -1,21 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "random.h"   // declares randchar()
 
+int main(void) {
+    int a;
 
+    srand((unsigned) time(NULL));
 
-int main()
-{
-	int a;
+    printf("Today's random word: ");
+    for (a = 0; a < 7; a++)
+        putchar(randchar());
+    putchar('\n');
 
-	/* seed the randomizer */
-	srand( (unsigned)time(NULL) );
-
-	printf("Today's random word: ");
-	for(a=0;a<7;a++)
-		putchar( randchar() );
-	putchar('\n');
-
-	return(0);
+    return 0;
 }
-
